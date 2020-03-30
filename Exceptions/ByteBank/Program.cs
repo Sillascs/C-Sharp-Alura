5299e2c9ca69457934949463dd964c2224122e94
Sillascs <59029146+Sillascs@users.noreply.github.com>
2020-03-30 12:27:01 -0300

    Inicio da parte 4: Entendendo exceções
    Conclusão do capitulo 1: Primeiros passos com exceções

---
+++ Exceptions/ByteBank/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Metodo();
+            Console.ReadLine();
+        }
+        //Teste com a cadeia de chamada:
+        //Metodo -> TestaDivisao -> Dividir
+        private static void Metodo()
+        {
+            try
+            {
+                TestaDivisao(0);
+            }
+            catch (NullReferenceException excecao)
+            {
+                Console.WriteLine(excecao.Message);
+                Console.WriteLine(excecao.StackTrace);
+            }
+        }
+
+        private static void TestaDivisao(int divisor)
+        {
+            try
+            {
+                int resultado = Dividir(10, divisor);
+                Console.WriteLine("Resultado da divisão de 10 por " + divisor + " é " + resultado);
+            }
+            catch (DivideByZeroException excecao)
+            {
+                Console.WriteLine(excecao.Message);
+                Console.WriteLine(excecao.StackTrace);
+            }
+        }
+
+        private static int Dividir(int numero, int divisor)
+        {
+            ContaCorrente conta = null;
+            Console.WriteLine(conta.Saldo);
+            return numero / divisor;
+        }
+    }
+}
