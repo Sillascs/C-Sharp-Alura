9801d0e63c0da6165278871d12ef6dd1014cd414
Sillascs <59029146+Sillascs@users.noreply.github.com>
2020-03-31 16:14:39 -0300

    Conclusão da parte 4: Entendendo exceções
    Conclusão do curso e do capitulo 6/6: Finally e using

---
+++ Exceptions/ByteBank/LeitorDeArquivo.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank
+{
+    //para utilizar o using na class Program é necessario utilizar a interface IDisposable
+    public class LeitorDeArquivo : IDisposable
+    {
+        public string Arquivo { get; }
+        public LeitorDeArquivo(string arquivo)
+        {
+            Arquivo = arquivo;
+            
+            //nessa exceção estamos simulando que o arquivo não foi encontrado
+            //throw new FileNotFoundException();
+            
+            Console.WriteLine("Abrindo arquivo: " + arquivo);
+        }
+
+        public string LerProximaLinha()
+        {
+            Console.WriteLine("Lendo linha...");
+
+            //IOException é as exceções de entrada e saida do sistema
+            //nesse exemplo estamos simulando um arquivo corrompido
+            //throw new IOException();
+            
+            return "Linha do arquivo";
+        }
+
+        //public void Fechar()
+        //{
+        //    Console.WriteLine("Fechando arquivo.");
+        //}
+
+        //por contrato da interface IDisposable temos que usar o construtor Dispose que fará a mesma função que o construtor Fechar()
+        //o Dispose é um metodo cuja a função é liberar os recursos
+        public void Dispose()
+        {
+            Console.WriteLine("Fechando arquivo.");
+        }
+    }
+}
--- Exceptions/ByteBank/Program.cs
+++ Exceptions/ByteBank/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,14 +13,71 @@ namespace ByteBank
         {
             try
             {
+                CarregarContas();
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("CATCH NO METODO MAIN");
+            }
+
+
+            //TestaInnerException();
+            //TestaExcecoes();
+            //DivisaoPorZero();
+
+            Console.WriteLine("Fim da execução aperter enter para sair.");
+            Console.ReadLine();
+        }
+
+        private static void CarregarContas()
+        {
+            //o using tem a mesma função de um try com finally
+            //e ele não libera os recursos do construtor Dispose quando seu valor é nulo  
+            using (LeitorDeArquivo leitor = new LeitorDeArquivo("text.txt"))
+            {
+                leitor.LerProximaLinha();
+            }
+
+            //------------------------------------------------------------------------------------------
+
+            //LeitorDeArquivo leitor = null;
+            ////a construção do try tem que ser com o catch ou finally ou ambos
+            //try
+            //{
+            //    leitor = new LeitorDeArquivo("contas.txt");
+
+            //    leitor.LerProximaLinha();
+            //    leitor.LerProximaLinha();
+            //    leitor.LerProximaLinha();
+            //    leitor.LerProximaLinha();
+            //}
+            //catch (IOException)
+            //{
+            //    Console.WriteLine("Exceção do tipo IOException capturada e tratada");
+            //}
+            ////finally será executado independente se foi achado uma exceção
+            //// estamos garantindo que mesmo que ao abrir o arquivo e achado uma exceção ele seja fechado
+            //finally
+            //{
+            //    Console.WriteLine("Executando o finally");
+            //    //com esse if estamos garantindo que só será fechado o arquivo q for encontrado e abreto
+            //    if(leitor != null)
+            //    {
+            //        leitor.Fechar();
+            //    }
+            //}
+        }
+
+        private static void TestaInnerException()
+        {
+            try
+            {
                 ContaCorrente conta = new ContaCorrente(253, 567385);
                 ContaCorrente conta2 = new ContaCorrente(344, 345464);
 
                 conta.Depositar(50);
-                //conta.Sacar(600);
-                //conta.Sacar(-10);
+                //conta.Sacar(600); //não tem a exceção InnerException no construtor que está utilizando, então quando for executar dará erro NullReferenceException
                 conta2.Transferir(1000, conta);
-                //conta2.Transferir(-10, conta);
             }
             //"e" ou "ex" é uma convenção usada para exceções
             catch (OperacaoFinanceiraException e)
@@ -43,6 +101,33 @@ namespace ByteBank
                 Console.WriteLine("Ocorreu uma exceção do tipo ArgumentException");
                 Console.WriteLine(ex.Message);
             }
+        }
+
+        private static void TestaExcecoes()
+        {
+            try
+            {
+                ContaCorrente conta = new ContaCorrente(253, 567385);
+                ContaCorrente conta2 = new ContaCorrente(344, 345464);
+
+                conta.Depositar(50);
+                conta.Sacar(600);
+                //conta.Sacar(-10);
+                //conta2.Transferir(-10, conta);
+            }
+            //"e" ou "ex" é uma convenção usada para exceções
+            catch (OperacaoFinanceiraException e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine(e.StackTrace);
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine("Argumento com problema " + ex.ParamName);
+                Console.WriteLine("Ocorreu uma exceção do tipo ArgumentException");
+                Console.WriteLine(ex.Message);
+            }
+            //***essa exceção foi usada antes de criar a OperacaoFinanceiraException que supre a utilização delas 
             //catch (SaldoInsuficientException ex)
             //{
             //    Console.WriteLine("Ocorreu uma exceção do tipo SaldoInsuficientException");
@@ -53,28 +138,26 @@ namespace ByteBank
             //    Console.WriteLine(e.Message);
             //    Console.WriteLine(e.StackTrace);
             //}
-
-            //try
-            //{
-            //    Metodo();
-            //}
-            //catch (DivideByZeroException e)
-            //{
-            //    Console.WriteLine("Não é possível divisão por 0");
-            //}
-            //catch (Exception e)
-            //{
-            //    Console.WriteLine(e.Message);
-            //    Console.WriteLine(e.StackTrace);
-            //}
-            
-            Console.ReadLine();
         }
-        private static void Metodo()
+
+
+        private static void DivisaoPorZero()
         {
-            TestaDivisao(0);
+            try
+            {
+                TestaDivisao(0);
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine("Não é possível divisão por 0");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine(e.StackTrace);
+            }
         }
-
+        
         private static void TestaDivisao(int divisor)
         {
             int resultado = Dividir(10, divisor);
