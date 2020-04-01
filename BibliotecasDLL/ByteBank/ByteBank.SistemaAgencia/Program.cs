e3e0f3cec80f59bf692c25f25e3d45c57201cd2e
Sillascs <59029146+Sillascs@users.noreply.github.com>
2020-04-01 19:04:43 -0300

    Inicio parte 5: Bibliotecas DLLs, documentação e usando NuGet
    Conclusão do capitulo 1/5: Projeto de biblioteca

---
+++ BibliotecasDLL/ByteBank/ByteBank.SistemaAgencia/Program.cs
@@ -0,0 +1,19 @@
+﻿using ByteBank.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank.SistemaAgencia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta = new ContaCorrente(245, 567889);
+            Console.WriteLine(conta.Saldo);
+            Console.ReadLine();
+        }
+    }
+}
