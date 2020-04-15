026ea88d1b56fe00f5539e1c170c9c4faab3386f
Sillascs <59029146+Sillascs@users.noreply.github.com>
2020-04-15 12:45:04 -0300

    Conclusão da parte 6: Strings, expressões regulares e a classe Object

---
+++ ProjetoManipulandoString/ByteBank.SistemaAgencia/Program.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using ByteBank.Modelos;
+using ByteBank.Modelos.Funcionarios;
+
+namespace ByteBank.SistemaAgencia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Trabalhando com Equals
+            Cliente carlos_1 = new Cliente();
+            carlos_1.Nome = "Carlos";
+            carlos_1.CPF = "458.623.120-03";
+            carlos_1.Profissao = "Designer";
+
+            Cliente carlos_2 = new Cliente();
+            carlos_2.Nome = "Carlos";
+            carlos_2.CPF = "458.623.120-03";
+            carlos_2.Profissao = "Designer";
+
+            //A resposta será que não são iguais pois estão em memória diferentes
+            if (carlos_1 == carlos_2)
+            {
+                Console.WriteLine("São iguais!");
+            }
+            else
+            {
+                Console.WriteLine("Não são iguais!");
+            }
+
+            //A resposta são iguais pois quando usamos o Equals, que está sobrescrito na classe de cliente
+            //manipulamos ele para verificar se os dados de cada instância são iguais
+            //qndo comparamos com outra classe que não seja a de cliente, tratei para que o retorno seja false e caia
+            //na condição de que não são iguais.
+            if (carlos_1.Equals(carlos_2))
+            {
+                Console.WriteLine("São iguais!");
+            }
+            else
+            {
+                Console.WriteLine("Não são iguais!");
+            }
+
+            Console.ReadLine();
+
+            //Trabalhando com ToString
+            // existem varias sobrecargar no Console.WriteLine()
+            Console.WriteLine("Olá, mundo!");
+            Console.WriteLine(123);
+            Console.WriteLine(10.5);
+            Console.WriteLine(true);
+
+            // todas as classes derivam de object
+            object conta = new ContaCorrente(456, 687876);
+            object desenvolvedor = new Desenvolvedor("4564654");
+
+            // por baixo dos panos o Console.WriteLine(conta) executa o ToString
+            // na classe ContaCorrente foi sobrescrita ToString para que apareça a mensagem que eu desejo
+            Console.WriteLine(conta);
+            Console.WriteLine(desenvolvedor);
+            
+            string contaToString = conta.ToString();
+
+            Console.WriteLine("Resultado " + contaToString);
+
+            Console.ReadLine();
+
+            // Olá, meu nome é Sillas e você pode entrar em contato comigo
+            // através do número 8457-4456!
+
+            // Meu nome é Sillas, me ligue em 4784-4546
+
+            //  "[0123456789][0123456789][0123456789][0123456789][-][0123456789][0123456789][0123456789][0123456789]";
+            //  "[0-9][0-9][0-9][0-9][-][0-9][0-9][0-9][0-9]";
+            //  "[0-9]{4,5}[-][0-9]{4}";
+            //  "[0-9]{4,5}[-]{0,1}[0-9]{4}";
+            //  "[0-9]{4,5}-{0,1}[0-9]{4}";
+            string padrao = "[0-9]{4,5}-?[0-9]{4}";
+
+            // 879.546.120-20
+            // 879546120-20
+
+            string textoDeTeste = "idyufdgfugfjksdhf 99871-5456 sdjkfgsdjgsjgh sfsdjgsdjghsdfj";
+
+            Match resultado = Regex.Match(textoDeTeste, padrao);
+
+            Console.WriteLine(resultado.Value);
+            Console.ReadLine();
+
+
+            string urlTeste = "https://www.bytebank.com/cambio";
+            int indiceByteBank = urlTeste.IndexOf("https://www.bytebank.com");
+
+            Console.WriteLine(urlTeste.StartsWith("https://www.bytebank.com"));
+            Console.WriteLine(urlTeste.EndsWith("cambio/"));
+
+            Console.WriteLine(urlTeste.Contains("ByteBank"));
+
+            Console.ReadLine();
+
+            // pagina?argumentos
+            // 012345678
+
+            //moedaOrigem=real&moedaDestino=dolar
+            //          |
+            //  -------´
+
+            string urlParametros = "http://www.bytebank.com/cambio?moedaOrigem=real&moedaDestino=dolar&valor=1500";
+            ExtratorValorDeArgumentosURL extratorDeValores = new ExtratorValorDeArgumentosURL(urlParametros);
+
+            string valor = extratorDeValores.GetValor("moedaDestino");
+            Console.WriteLine("Valor de moedaDestino: " + valor);
+
+            string valorMoedaOrigem = extratorDeValores.GetValor("moedaOrigem");
+            Console.WriteLine("Valor de moedaOrigem: " + valorMoedaOrigem);
+
+            Console.WriteLine(extratorDeValores.GetValor("VALOR"));
+
+            Console.ReadLine();
+
+
+            //Testando ToLower
+            string mensagemOrigem = "PALAVRA";
+            string termoBusca = "ra";
+
+            Console.WriteLine(mensagemOrigem.ToLower());
+
+
+            // Testando Replace
+            termoBusca = termoBusca.Replace('r', 'R');
+            Console.WriteLine(termoBusca);
+
+            termoBusca = termoBusca.Replace('a', 'A');
+            Console.WriteLine(termoBusca);
+
+            Console.WriteLine(mensagemOrigem.IndexOf(termoBusca));
+            Console.ReadLine();
+
+            // Testando o método Remove
+            string testeRemocao = "primeiraParte&123456789";
+            int indiceEComercial = testeRemocao.IndexOf('&');
+            Console.WriteLine(testeRemocao.Remove(indiceEComercial, 4));
+            Console.ReadLine();
+
+
+            // <nome>=<valor>
+            string palavra = "moedaOrigem=moedaDestino&moedaDestino=dolar";
+            string nomeArgumento = "moedaDestino=";
+
+            int indice = palavra.IndexOf(nomeArgumento);
+            Console.WriteLine(indice);
+
+            Console.WriteLine("Tamanho da string nomeArgumento: " + nomeArgumento.Length);
+
+            Console.WriteLine(palavra);
+            Console.WriteLine(palavra.Substring(indice));
+            Console.WriteLine(palavra.Substring(indice + nomeArgumento.Length));
+            Console.ReadLine();
+
+
+            // Testando o IsNullOrEmpty
+            string textoVazio = "";
+            string textoNulo = null;
+            string textoQualquer = "kjhfsdjhgsdfjksdf";
+            Console.WriteLine(String.IsNullOrEmpty(textoVazio));
+            Console.WriteLine(String.IsNullOrEmpty(textoNulo));
+            Console.WriteLine(String.IsNullOrEmpty(textoQualquer));
+            Console.ReadLine();
+
+            //// testando a exceção de argumento vazio
+            //ExtratorValorDeArgumentosURL extrator = new ExtratorValorDeArgumentosURL("");
+            //Console.ReadLine();
+
+            string url = "pagina?moedaOrigem=real&moedaDestino=dolar";
+
+            int indiceInterrogacao = url.IndexOf('?');
+
+            Console.WriteLine(indiceInterrogacao);
+
+            Console.WriteLine(url);
+            string argumentos = url.Substring(indiceInterrogacao + 1);
+            Console.WriteLine(argumentos);
+            Console.ReadLine();
+        }
+    }
+
+
+}
